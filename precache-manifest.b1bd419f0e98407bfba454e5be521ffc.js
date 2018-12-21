67ff690539e86d956d9565fa9d4fdd1ea4dbda64
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2018-12-21 10:50:27 +0100

    Updates

---
+++ precache-manifest.b1bd419f0e98407bfba454e5be521ffc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ea2afcd73af9dfa8a46",
+    "url": "/static/js/main.5ea2afcd.chunk.js"
+  },
+  {
+    "revision": "092feaf662e5bf7a6229",
+    "url": "/static/js/1.092feaf6.chunk.js"
+  },
+  {
+    "revision": "5ea2afcd73af9dfa8a46",
+    "url": "/static/css/main.af43de63.chunk.css"
+  },
+  {
+    "revision": "8c8c5335212794d7cdaf481e2765c447",
+    "url": "/index.html"
+  }
+];
